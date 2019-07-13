9c56d893b6085b91be1081101a1be83467711f26
SubhoBasak <subhobasak50@gmail.com>
2019-07-13 18:22:37 +0530

    added weighted graph (directed and undirected both)

---
+++ 13_Weighted_graph/Weighted_graph(undirected).py
@@ -0,0 +1,37 @@
+class Vertex:
+    def __init__(self, name, value = None):
+        self.name = name
+        self.value = value
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+        self.edges = []
+        self.indx = {}
+        self.rev_indx = {}
+
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+            self.vertices[vertex.name] = vertex
+            for row in self.edges:
+                row.append(0)
+            self.edges.append([0]*(len(self.edges)+1))
+            self.indx[vertex.name] = len(self.indx)
+            self.rev_indx[len(self.rev_indx)] = vertex.name
+        else:
+            print('Something went wrong !')
+
+    def add_edges(self, u, v, weight):
+        if u in self.vertices and v in self.vertices:
+            self.edges[self.indx[u]][self.indx[v]] = weight
+            self.edges[self.indx[v]][self.indx[u]] = weight
+        else:
+            print('Vertices are not in graph !')
+
+    def show_graph(self):
+        for i in self.vertices:
+            print(self.vertices[i].name)
+            for j, k in enumerate(self.edges[self.indx[i]]):
+                if k > 0:
+                    print('\t', self.vertices[self.rev_indx[j]].name, k)
+            print('==========')
